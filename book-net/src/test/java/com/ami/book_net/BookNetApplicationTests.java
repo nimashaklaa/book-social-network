d69d7ed86b48593bddc0b810cc7882a3cda8f9d7
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-10 10:11:15 +0530

    Initialize Spring Boot application for Book Social Network, including Maven setup, basic configurations, PostgreSQL integration, and initial user entity.

---
+++ book-net/src/test/java/com/ami/book_net/BookNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ami.book_net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
