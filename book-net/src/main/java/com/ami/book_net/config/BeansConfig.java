b2262aa534282cc2441c1fb130dc940368d757c5
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-11 21:32:15 +0530

    Add AuthenticationProvider bean with BCryptPasswordEncoder for improved security configuration

--- book-net/src/main/java/com/ami/book_net/config/BeansConfig.java
+++ book-net/src/main/java/com/ami/book_net/config/BeansConfig.java
@@ -1,4 +1,26 @@
 package com.ami.book_net.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
 public class BeansConfig {
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
