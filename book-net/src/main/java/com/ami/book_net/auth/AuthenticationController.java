e0e382b559fe7d2b6778499a5b2521086fd7ec52
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-13 14:41:04 +0530

    Update dependencies (jjwt 0.12.6, springdoc 2.8.9, commons-lang3 3.20.0), add AuthenticationController, and refine application.yml for cleaner configuration

---
+++ book-net/src/main/java/com/ami/book_net/auth/AuthenticationController.java
@@ -0,0 +1,10 @@
+package com.ami.book_net.auth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+}
