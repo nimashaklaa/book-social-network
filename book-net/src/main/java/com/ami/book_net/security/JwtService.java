f5c0bdd1bb6a69d70468f1d47890659529747e71
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-13 18:05:43 +0530

    Refactor `JwtService` for cleaner API usage and stronger type safety.

--- book-net/src/main/java/com/ami/book_net/security/JwtService.java
+++ book-net/src/main/java/com/ami/book_net/security/JwtService.java
@@ -9,7 +9,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.function.Function;
@@ -39,11 +40,11 @@ public class JwtService {
 
     private Claims extractAllClaims(String token) {
         return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey())
+                .parser()
+                .verifyWith(getSignInKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public String generateToken(HashMap<String, Object> claims, UserDetails userDetails) {
@@ -62,10 +63,10 @@ public class JwtService {
                 .toList();
         return Jwts
                 .builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
                 .claim("authorities", authorities)
                 .signWith(getSignInKey())
                 .compact();
@@ -86,9 +87,8 @@ public class JwtService {
     }
 
 
-    private Key getSignInKey() {
-
-        byte[] keyBytes   = Decoders.BASE64.decode(secretKey);
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
