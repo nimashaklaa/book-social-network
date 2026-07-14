424013aa30399c8c56406bae3d7ba80bab145524
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-14 11:45:52 +0530

    Add `Book`, `Feedback`, and `BaseEntity` models with JPA annotations to define core entities and extend auditing capabilities.

---
+++ book-net/src/main/java/com/ami/book_net/book/Book.java
@@ -0,0 +1,26 @@
+package com.ami.book_net.book;
+
+import com.ami.book_net.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+}
---
+++ book-net/src/main/java/com/ami/book_net/feedback/Feedback.java
@@ -0,0 +1,23 @@
+package com.ami.book_net.feedback;
+
+import com.ami.book_net.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double rating;
+    private String comment;
+
+}
