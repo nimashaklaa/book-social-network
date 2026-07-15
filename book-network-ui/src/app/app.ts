567f177d8aae7aaa7ad04932d68ee6d0c92e1f5b
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-15 20:39:24 +0530

    Replace placeholder HTML content with a `router-outlet` and integrate Bootstrap for styling

--- book-network-ui/src/app/app.ts
+++ book-network-ui/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // ← this is the HTML tag name
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  templateUrl: './app.html', // ← its HTML
   styleUrl: './app.css'
 })
+//It's metadata that tells Angular "this class is a component"
 export class App {
   protected readonly title = signal('book-network-ui');
 }
