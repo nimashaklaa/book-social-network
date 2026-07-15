8958ab6e104ade75d43e3e4118b921d324575c48
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-15 12:18:49 +0530

    Add repository query to fetch feedback by book ID

--- book-net/src/main/java/com/ami/book_net/feedback/FeedbackRepository.java
+++ book-net/src/main/java/com/ami/book_net/feedback/FeedbackRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Integer>, JpaSpecificationExecutor<Feedback> {
+    @Query("""
+    SELECT feedback
+    FROM Feedback feedback
+    WHERE feedback.book.id = :id
+    """)
     Page<Feedback> findAllByBookId(Pageable pageable, Integer id);
 }
