4cbb96fc5bee9f5ecc6ff3501169cd710c83ade2
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-16 11:35:11 +0530

    feat: refactor account activation logic to use signals for state management and improve error handling

--- book-network-ui/src/app/pages/activate-account/activate-account.ts
+++ book-network-ui/src/app/pages/activate-account/activate-account.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {ApiConfiguration} from '../../services/api-configuration';
@@ -13,19 +13,35 @@ import {CodeInputModule} from 'angular-code-input';
   styleUrl: './activate-account.scss',
 })
 export class ActivateAccount {
-  message:string = '';
-  isOkay:boolean = true;
-  submitted:boolean = false;
+  message = signal<string>('');
+  isOkay = signal<boolean>(true);
+  submitted = signal<boolean>(false);
 
   private router = inject(Router);
   private http = inject(HttpClient);
   private apiConfig = inject(ApiConfiguration);
 
   protected onCodeCompleted(token: string) {
-
+    this.confirmAccount(token);
   }
 
   protected async redirectedToLogin() {
     await this.router.navigate(['/login']);
+  }
+
+  private confirmAccount(token: string) {
+    this.http.get(`${this.apiConfig.rootUrl}/auth/activate-account?token=${token}`).subscribe({
+      next: async () => {
+        this.message.set('Account activated successfully');
+        this.isOkay.set(true);
+        this.submitted.set(true);
+      },
+      error: (err) => {
+        const body = typeof err.error === 'string' ? JSON.parse(err.error) : err.error;
+        this.message.set(body?.error ?? 'Activation failed');
+        this.isOkay.set(false);
+        this.submitted.set(true);
+      }
+    });
   }
 }
