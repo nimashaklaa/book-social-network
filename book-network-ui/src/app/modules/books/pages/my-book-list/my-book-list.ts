4225da946a587d7af3c4a14dc94e155a67229247
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-16 18:21:48 +0530

    feat: add offcanvas drawer for adding new books with form validation

--- book-network-ui/src/app/modules/books/pages/my-book-list/my-book-list.ts
+++ book-network-ui/src/app/modules/books/pages/my-book-list/my-book-list.ts
@@ -6,10 +6,12 @@ import { BookResponse } from '../../../../services/models/book-response';
 import { PageResponseBookResponse } from '../../../../services/models/page-response-book-response';
 import { Pagination } from '../../components/pagination/pagination';
 import { BasePage } from '../base-page';
+import {BookRequest} from '../../../../services/models/book-request';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-my-book-list',
-  imports: [DecimalPipe, Pagination],
+  imports: [DecimalPipe, Pagination, FormsModule],
   templateUrl: './my-book-list.html',
   styleUrl: './my-book-list.scss',
 })
@@ -19,6 +21,15 @@ export class MyBookList extends BasePage implements OnInit {
 
   books = signal<Array<BookResponse>>([]);
   errorMessage = signal<Array<string>>([]);
+  isDrawerOpen = signal<boolean>(false);
+
+  newBook: BookRequest ={
+    title: '',
+    authorName: '',
+    isbn: '',
+    synopsis: '',
+    shareable: true
+  }
 
   ngOnInit() {
     this.loadData();
@@ -34,8 +45,30 @@ export class MyBookList extends BasePage implements OnInit {
         this.setPageData(response.first ?? true, response.last ?? false, response.totalPages ?? 0);
       },
       error: (err) => {
-        const body = typeof err.error === 'string' ? JSON.parse(err.error) : err.error;
-        this.errorMessage.set(body?.validationErrors ?? (body?.error ? [body.error] : ['An error occurred']));
+        this.handleError(err)
+      }
+    });
+  }
+  protected addNewBook() {
+    this.resetForm();
+    this.isDrawerOpen.set(true);
+  }
+  protected closeDrawer() {
+    this.isDrawerOpen.set(false);
+  }
+
+  protected submitNewBook() {
+    this.http.post<number>(
+      `${this.apiConfig.rootUrl}/books`,
+      this.newBook
+    ).subscribe({
+      next: (response) => {
+        this.closeDrawer();
+        this.resetForm();
+        this.loadData();
+      },
+      error: (err) => {
+        this.handleError(err)
       }
     });
   }
@@ -44,4 +77,19 @@ export class MyBookList extends BasePage implements OnInit {
   protected updateBookShareableStatus() {}
   protected updateBookArchivedStatus() {}
   protected updateCoverPicture() {}
+
+
+  private resetForm() {
+    this.newBook = {
+      title: '',
+      authorName: '',
+      isbn: '',
+      synopsis: '',
+      shareable: true,
+    }
+  }
+  private handleError(err: any) {
+    const body = typeof err.error === 'string' ? JSON.parse(err.error) : err.error;
+    this.errorMessage.set(body?.validationErrors ?? (body?.error ? [body.error] : ['An error occurred']));
+  }
 }
