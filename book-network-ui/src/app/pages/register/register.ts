eb9c245c9600d545337fe4848143ba88a437a065
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-07-16 10:34:41 +0530

    feat: add register page with routing, UI, form handling, and skeleton logic

--- book-network-ui/src/app/pages/register/register.ts
+++ book-network-ui/src/app/pages/register/register.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {RegistrationRequest} from '../../services/models/registration-request';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  registrationRequest: RegistrationRequest ={
+    email:'',
+    firstname:'',
+    lastname:'',
+    password:'',
+  }
+  errorMessage = signal<Array<string>>([]);
 
+
+  protected register() {
+
+  }
+
+  protected login() {
+
+  }
 }
